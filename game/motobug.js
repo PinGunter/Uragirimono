4daba1aca3a17074aed50edf652f3be142e3408b
Salva Romero <salvaromero08@gmail.com>
2022-05-16 22:01:42 +0200

    cabezita hecha

--- game/motobug.js
+++ game/motobug.js
@@ -6,9 +6,15 @@ class Motobug extends THREE.Object3D{
     constructor(){
         super();
         var ruedaGeo = new THREE.TorusGeometry(5, 2.75, 16, 100);
-        var ruedaMat = new THREE.MeshToonMaterial({color: "grey"});
+        var texture = new THREE.TextureLoader().load("../imgs/tire.jpg");
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(1, 1);
+        var ruedaMat = new THREE.MeshToonMaterial({color: "grey", map: texture});
 
         this.rueda = new THREE.Mesh(ruedaGeo, ruedaMat);
+
+
 
         var curvaCuerpo = new THREE.EllipseCurve(
             0,0,
@@ -34,14 +40,45 @@ class Motobug extends THREE.Object3D{
 
         var cuerpo = new CSG();
         cuerpo.union([cuerpoCentral, cuerpoLat1]); 
+        this.cuerpoFinal = cuerpo.toMesh();
+      
+        
+        
+        var pathCabeza = new THREE.EllipseCurve(
+            0,0,
+            5.5,5.5,
+            Math.PI/3, 0,
+            true,
+            0
+        )       
+        puntos2d = pathCabeza.getPoints(50);
+        var formaCabeza = new THREE.Shape(curvaCuerpo.getPoints(50));
+        puntos3d = [];
+        puntos2d.forEach(p => {
+            puntos3d.push(new THREE.Vector3(p.x, p.y, 0));
+        });
+        path = new THREE.CatmullRomCurve3(puntos3d);
+        var cabezaGeo = new THREE.ExtrudeGeometry(formaCabeza, {extrudePath: path, steps: 50});
+        var cabezaMat = new THREE.MeshToonMaterial({color: "cyan"});
+        this.cabeza = new THREE.Mesh(cabezaGeo, cabezaMat);
+
+
+        // var cabezaGeo = new THREE.SphereGeometry(4.5,16,16,0,2*Math.PI,0,Math.PI);
+        // var cabezaMat = new THREE.MeshToonMaterial({color: "cyan"}); 
+        // cabezaGeo.scale(1,0.5,1);
+        // cabezaGeo.rotateZ(Math.PI/4);
+        // cabezaGeo.translate(-9,5.5,0);
+        // this.cabeza = new THREE.Mesh(cabezaGeo, cabezaMat);
+
         this.add(this.rueda);
-        this.add(cuerpo.toMesh());       
+        this.add(this.cuerpoFinal);       
+        this.add(this.cabeza);
     }
 
 
     
     update(){
-        // this.rotation.y += 0.01;
+        this.rueda.rotation.z -= 0.01;
     }
 }
 
